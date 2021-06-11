03db21b06469efa9ca5a5aa7b45408102db3682a
Ollie <olliemoss919@gmail.com>
2021-06-11 09:37:58 +0000

    delete old images file and add images folder as replacement

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const navSlide = () =>{
+const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document. querySelector('.nav-links');
-    
-    burger.addEventListener('click', ()=> {
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+    });
+
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkfade 0.5s ease forwards ${index / 7}s`
+        console.log(index / 5);
     });
 }
 
-navSlide(); 
+navSlide();
